cfa803467bfeca044cb17e5b4bb6c0c8c8a0334b
Javohir-codes <jo2011baxti@gmail.com>
2026-03-11 11:36:27 +0500

    orders

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(blank=True, null=True)
+
+    product_name = models.CharField(max_length=200)
+
+    message = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import OrderForm
 
-# Create your views here.
+
+def create_order(request):
+
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('order_success')
+
+    else:
+        form = OrderForm()
+
+    return render(request, "orders/order_form.html", {"form": form})
+
+
+def order_success(request):
+    return render(request, "orders/success.html")
