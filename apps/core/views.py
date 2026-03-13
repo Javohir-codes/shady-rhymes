1da113f1e5a562351a4b090fbc3b241e06991bc4
Javohir-codes <jo2011baxti@gmail.com>
2026-03-13 13:44:56 +0500

    products

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 from apps.comment.models import Comment
 
 
 def home(request):
 
+    products = Product.objects.all()[:4]
+
     comments = Comment.objects.all().order_by("-created_at")[:4]
 
-    return render(request, "core/home.html", {
+    context = {
+        "products": products,
         "comments": comments
-    })
+    }
+
+    return render(request, "core/home.html", context)
